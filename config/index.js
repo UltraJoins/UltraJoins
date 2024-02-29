c89f7bf313301cd5f2876e19ac57d64fcd6a81a4
Ceed#2117 <82039844+ceedledev@users.noreply.github.com>
2024-02-29 18:24:44 +0100

    Add files via upload

---
+++ config/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    api: require("./api.json"),
+    bot: require("./bot.json"),
+    color: require("./color.json"),
+    emojis: require("./emojis.json"),
+    supportGuild: "https://discord.gg/ultrajoins",
+    guildId: "guild id",
+    unbanGuild: "no.",
+    imageBot: "https://media.discordapp.net/attachments/1167048211819675669/1168153578460946542/IMG_0115.png?ex=6550bacc&is=653e45cc&hm=fa4637d3197437febf270a7cb750a7c128f6350162ab243f16a8ead0a4b55f5a&",
+    ownersId: "1018098085227933767"
+};
